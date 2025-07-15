7b5da8c8bc4608497001b6537cbfab78f380ee4f
Sikun275 <sikun275@outlook.com>
2025-07-15 14:51:40 -0400

    Initial Commit

---
+++ DecideForMe/DecideForMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecideForMeApp.swift
+//  DecideForMe
+//
+//  Created by Sikun Chen on 2025-07-15.
+//
+
+import SwiftUI
+
+@main
+struct DecideForMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DecideForMeTests/DecideForMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DecideForMeTests.swift
+//  DecideForMeTests
+//
+//  Created by Sikun Chen on 2025-07-15.
+//
+
+import Testing
+@testable import DecideForMe
+
+struct DecideForMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
